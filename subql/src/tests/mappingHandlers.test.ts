849d946f8e748efa04df5c8b67df92f36474b50a
Thomas <thomasguntenaar@gmail.com>
2024-09-09 10:53:04 -0500

    add subql testing framework

---
+++ subql/src/tests/mappingHandlers.test.ts
@@ -0,0 +1,26 @@
+import { subqlTest } from "@subql/testing";
+import { NearActionEntity } from "../types";
+
+subqlTest(
+  "handleEvent test", // test name
+  1000003, // block height to process
+  [
+    NearActionEntity.create({
+      id: "0x0ca3c88eaa25af380f243273e83ba2e161b207d935357af063e2bd5b8a2e9c40", // Replace this with the actual block hash for block 103
+      // field1: 1000003,
+      // id: string,
+      sender: "thomasguntenaar.near",
+      receiver: "devhub.near",
+      amount: BigInt(10000000), // Changed amount to BigInt
+    }),
+  ], // dependent entities
+  [
+    NearActionEntity.create({
+      id: "0x0ca3c88eaa25af380f243273e83ba2e161b207d935357af063e2bd5b8a2e9c40", // Replace this with the actual block hash for block 103
+      sender: "thomasguntenaar.near",
+      receiver: "devhub.near",
+      amount: BigInt(10000000), // Changed amount to BigInt
+    }),
+  ], // expected entities
+  "handleEvent" //handler name
+);
